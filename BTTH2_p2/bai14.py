2208939fe5921d2de1a406e442449566195814ff
DucThacc <ducthanh300904@gmail.com>
2025-09-22 21:46:30 +0700

    .

--- BTTH2_p2/bai14.py
+++ BTTH2_p2/bai14.py
@@ -3,3 +3,11 @@
 In các số tìm được thành chuỗi cách nhau bởi dấu phẩy, trên một dòng. 
 """
 
+values = []
+for i in range(100, 301):
+    s = str(i)
+    if (int(s[0])%2 ==0 and int(s[1])%2 ==0 and int(s[2])%2 ==0 ):
+        values.append(s)
+
+print(",".join(values))
+
---
+++ BTTH2_p2/bai15.py
@@ -0,0 +1,5 @@
+"""Bài 15: Viết chương trình nhập một chuỗi số, phân tách bằng dấu phẩy. In ra một danh sách và một tuple chứa mọi số.
+Ví dụ: Đầu vào được cung cấp là 34,67,55,33,12,98 thì đầu ra là:
+["34", "67", "55", "33", "12", "98"]
+("34", "67", "55", "33", "12", "98")
+"""
