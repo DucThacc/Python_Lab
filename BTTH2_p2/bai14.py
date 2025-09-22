dd6f4fd9ddbccdae7467a8ff5225507be639c5a5
DucThacc <ducthanh300904@gmail.com>
2025-09-22 20:49:34 +0700

    week2

---
+++ BTTH2_p2/bai14.py
@@ -0,0 +1,5 @@
+"""Bài 14: Viết một chương trình tìm tất cả các số trong đoạn 
+100 và 300 (tính cả 2 số này) sao cho tất cả các chữ số trong số đó là số chẵn. 
+In các số tìm được thành chuỗi cách nhau bởi dấu phẩy, trên một dòng. 
+"""
+
