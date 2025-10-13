468c20477ab99a324c9f99b26b37ceeeff384034
DucThacc <ducthanh300904@gmail.com>
2025-10-13 21:47:56 +0700

    all

--- BTTH2_p2/bai15.py
+++ BTTH2_p2/bai15.py
@@ -1,5 +1,11 @@
 """Bài 15: Viết chương trình nhập một chuỗi số, phân tách bằng dấu phẩy. In ra một danh sách và một tuple chứa mọi số.
-Ví dụ: Đầu vào được cung cấp là 34,67,55,33,12,98 thì đầu ra là:
+Ví dụ: Đầu vào được cung cấp là 34,67,55,33,12,98 thì đầu ra là:
 ["34", "67", "55", "33", "12", "98"]
 ("34", "67", "55", "33", "12", "98")
 """
+
+chuoi=input("Nhập vào 1 chuỗi: ")
+kieu_ds=chuoi.split(",")
+kieu_turples = tuple(kieu_ds)
+print(kieu_ds)
+print(kieu_turples)
